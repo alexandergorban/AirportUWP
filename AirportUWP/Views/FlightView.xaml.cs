faff4d6294ff4ad3164efb81058490189d23e1e8
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-07-31 14:02:45 +0300

    Added FlightView

--- AirportUWP/Views/FlightView.xaml.cs
+++ AirportUWP/Views/FlightView.xaml.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using AirportUWP.Models;
+using AirportUWP.Services;
 
 namespace AirportUWP.Views
 {
-    class FlightView
+    public sealed partial class FlightView : Page
     {
+        private readonly FlightService _flightService;
+        public ObservableCollection<FlightDto> FlightDtos { get; set; }
+
+        public FlightView()
+        {
+            this.InitializeComponent();
+
+            _flightService = new FlightService();
+            FlightDtos = new ObservableCollection<FlightDto>();
+
+            AirplaneTypesList.ItemsSource = FlightDtos;
+        }
+
+        public async void GetEntities(object sender, RoutedEventArgs e)
+        {
+            await LoadDepartureAsync();
+        }
+
+        private async Task LoadDepartureAsync()
+        {
+            foreach (var airplaneType in await _flightService.GetEntitiesAsync())
+            {
+                FlightDtos.Add(airplaneType);
+            }
+        }
     }
 }
